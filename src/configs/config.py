ad4cb21b902125c945d6d514f0f4a84a6ec608ac
Callet91 <calle.jensen@outlook.com>
2021-02-14 23:47:39 +0100

    Added config file for model

---
+++ src/configs/config.py
@@ -0,0 +1,11 @@
+'''Config file for example model'''
+
+example_config = {
+    'activation': 'relu',
+    'batch': 32, 
+    'epochs': 5, 
+    'loss': 'sparse_categorical_crossentropy',
+    'metrics': 'accuracy',  
+    'optimizer': 'adam'
+    
+}
